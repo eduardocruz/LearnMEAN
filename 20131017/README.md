959cb7d29fefa08f9f1b8f2eeb311b9b2a1b4c46
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-10-18 01:55:27 -0300

    Day 1
    Egghead.io - Video 1 - Binding

---
+++ 20131017/README.md
@@ -0,0 +1,3 @@
+Day 1
+=========
+17 Oct 2013 - Started creating a Angular.js project using Yeoman and following the first video of Egghead.io
---
+++ 20131017/app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('angularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'Yeoman - http://yeoman.io',
+      'AngularJS Video Tutorial: Binding - http://egghead.io/lessons/angularjs-binding'
+    ];
+  });
